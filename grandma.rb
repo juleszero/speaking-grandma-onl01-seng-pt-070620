de9a5c67770dee11eeffa3be0fabe91fb77addab
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-07 20:43:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
-   if phrase == "I LOVE YOU GRANDMA!"
-     return "I LOVE YOU TOO PUMPKIN!"
-   elsif phrase == phrase.upcase
-     return "NO, NOT SINCE 1938"
-   else 
-     return "HUH?! SPEAK UP, SONNY!"
-end
+  if phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH, SPEAK UP SONNY!"
+  end
